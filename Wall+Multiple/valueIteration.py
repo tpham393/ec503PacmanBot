9ebfe06074cce05099af620bf04b33c3ce9d4ff5
jh97 <jhua@bu.edu>
2019-12-09 19:56:22 -0500

    making sure everything is up to date

--- Wall+Multiple/valueIteration.py
+++ Wall+Multiple/valueIteration.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     # Init
     col, row =7,7;
     num_ghosts = 2;
-    game = Game(7,7);
+    game = Game();
     env = PacmanEnv(num_ghosts=num_ghosts, ghost_type=['Chase','Random'], grid_len=7, pellet_x=1, pellet_y=5, grid=game.grid);
     policy, v, steps = value_iteration(env, gamma=0.5);
 
