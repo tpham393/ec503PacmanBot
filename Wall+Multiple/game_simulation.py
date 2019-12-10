d644dc8593cc7016455680999196b2358cac2e08
tpham393 <thuyp@bu.edu>
2019-12-09 20:33:21 -0500

    added calculation for avg # steps to win

--- Wall+Multiple/game_simulation.py
+++ Wall+Multiple/game_simulation.py
@@ -52,8 +52,11 @@ while line:
 ############################  Game Simulation #################################
 ###############################################################################
 win_count = 0
+winning_steps = []
+
 for s in range(5): # run 50 simulations
     game = Game();    
+    steps_to_win = 0
 
     ghostType = ['Chase','Random'];
     pacman_x,pacman_y = 5,1;
@@ -69,6 +72,7 @@ for s in range(5): # run 50 simulations
         state = coord2state(game.pacman_x, game.pacman_y, game.ghost_x, game.ghost_y, num_ghosts, grid_len);
         # My policy[state] outputs 0 to 3, that's why I add 1, because Steven's actions go 1 to 4
         move = policy[state] + 1;
+        steps_to_win += 1
 
         # Get game status given current state and move (action)
         pacman_x2, pacman_y2, ghost_x2, ghost_y2, game.goal_x, game.goal_y, game.ended, game.won = g.game_func(move, game.pacman_x, game.pacman_y, game.ghost_x, game.ghost_y, game.goal_x, game.goal_y, game.grid, 2, ghostType);
@@ -79,8 +83,10 @@ for s in range(5): # run 50 simulations
 
     if (game.won):
         win_count += 1
+        winning_steps.append(steps_to_win) # does not add to list if game is lost
 
 print('Total # of wins: ', win_count)
+print('Average # steps to win: ', np.mean(winning_steps))
 pygame.display.quit()
 pygame.quit()
 sys.exit()
