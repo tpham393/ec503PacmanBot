39f6691b6b373ab30d1325f5bea796394a8b4a23
tpham393 <thuyp@bu.edu>
2019-12-09 20:29:53 -0500

    tweaked code for quick simulations

--- Wall+Multiple/game_simulation.py
+++ Wall+Multiple/game_simulation.py
@@ -3,7 +3,8 @@ from random import randint as ri
 from game import Game
 import game_funcs as g
 import math
-
+import pygame
+import sys
 
 def coord2state(pacmanLocX, pacmanLocY, ghostLocX, ghostLocY, num_ghosts, grid_len):
     '''
@@ -50,26 +51,36 @@ while line:
 ###############################################################################
 ############################  Game Simulation #################################
 ###############################################################################
-game = Game();    
+win_count = 0
+for s in range(5): # run 50 simulations
+    game = Game();    
 
-ghostType = ['Chase','Random'];
-pacman_x,pacman_y = 5,1;
-ghost_x = [2, 3];
-ghost_y = [5, 1];
-game.updateState(pacman_x, pacman_y, ghost_x, ghost_y, num_ghosts, ghostType) # update internal grid
+    ghostType = ['Chase','Random'];
+    pacman_x,pacman_y = 5,1;
+    ghost_x = [2, 3];
+    ghost_y = [5, 1];
+    game.updateState(pacman_x, pacman_y, ghost_x, ghost_y, num_ghosts, ghostType) # update internal grid
 
-## Start game
-while not game.ended:
-    game.update(); # update graphics
-    time.sleep(0.5);
-    # Get state (0 to numStates-1) from pacman and ghost coordinates
-    state = coord2state(game.pacman_x, game.pacman_y, game.ghost_x, game.ghost_y, num_ghosts, grid_len);
-    # My policy[state] outputs 0 to 3, that's why I add 1, because Steven's actions go 1 to 4
-    move = policy[state] + 1;
+    ## Start game
+    while not game.ended:
+        game.update(); # update graphics
+        time.sleep(0.05);
+        # Get state (0 to numStates-1) from pacman and ghost coordinates
+        state = coord2state(game.pacman_x, game.pacman_y, game.ghost_x, game.ghost_y, num_ghosts, grid_len);
+        # My policy[state] outputs 0 to 3, that's why I add 1, because Steven's actions go 1 to 4
+        move = policy[state] + 1;
 
-    # Get game status given current state and move (action)
-    pacman_x2, pacman_y2, ghost_x2, ghost_y2, game.goal_x, game.goal_y, game.ended, game.won = g.game_func(move, game.pacman_x, game.pacman_y, game.ghost_x, game.ghost_y, game.goal_x, game.goal_y, game.grid, 2, ghostType);
-    game.moves.append(move);
-    game.updateState(pacman_x2, pacman_y2, ghost_x2, ghost_y2, num_ghosts, ghostType); # update internal grid
-# Update one last time before game end
-game.update(); 
+        # Get game status given current state and move (action)
+        pacman_x2, pacman_y2, ghost_x2, ghost_y2, game.goal_x, game.goal_y, game.ended, game.won = g.game_func(move, game.pacman_x, game.pacman_y, game.ghost_x, game.ghost_y, game.goal_x, game.goal_y, game.grid, 2, ghostType);
+        game.moves.append(move);
+        game.updateState(pacman_x2, pacman_y2, ghost_x2, ghost_y2, num_ghosts, ghostType); # update internal grid
+    # Update one last time before game end
+    game.update(); 
+
+    if (game.won):
+        win_count += 1
+
+print('Total # of wins: ', win_count)
+pygame.display.quit()
+pygame.quit()
+sys.exit()
